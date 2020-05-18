8f000713f405e17aefa7b907956bb95886d3b42c
caicindy87 <caicindy87@gmail.com>
2020-05-18 10:05:18 -0700

    Done.

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,4 +1,15 @@
 function insertionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  for (let i = 1; i < arr.length; i++) {
+    let key = arr[i];
+    let j = i - 1;
+
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+    arr[j + 1] = key;
+  }
+  return arr;
 }
--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,19 @@
 function selectionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+
+  for (i = 0; i < arr.length; i++) {
+    let min = i;
+    for (j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    if (min !== i) {
+      let tmp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = tmp;
+    }
+  }
+  return arr;
 }
